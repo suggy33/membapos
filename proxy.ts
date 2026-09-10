6843416e0bc12032a83bf943a95c0ef58e177314
suggy33 <saggar.co@gmail.com>
2026-09-10 20:22:55 +1000

    updated middleware

--- proxy.ts
+++ proxy.ts
@@ -1,6 +1,8 @@
 import { clerkMiddleware, createRouteMatcher } from "@clerk/nextjs/server"
 
 const isPublicRoute = createRouteMatcher([
+  "/",
+  "/login(.*)",
   "/sign-in(.*)",
   "/sign-up(.*)",
   "/api/webhooks(.*)",
